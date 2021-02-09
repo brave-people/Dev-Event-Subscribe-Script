ff6f074518ee020c0a8ad47712f0ff54c615e4b4
Limm-jk <201602057@cs-cnu.org>
2021-02-09 23:56:35 +0900

    [feat] split event html
    html 중 이벤트 부분을 찾아서 반환

--- event_crawling.py
+++ event_crawling.py
@@ -13,3 +13,15 @@ def get_html(url) -> str:
     """
     response = requests.get(url)
     return response.text
+
+def split_event_html(html, range = 20):
+    """    
+    html 중 이벤트 부분을 찾아서 반환
+    두달치로 고정
+    param html -> 페이지의 html / str
+    param range -> event의 위치 / int
+    return soup Object List
+    """
+    br_split_HTML = list(html.split('<br>')[range:])
+    soup = BeautifulSoup(br_split_HTML[0] + br_split_HTML[1], 'html.parser')
+    return soup.findAll("li")
