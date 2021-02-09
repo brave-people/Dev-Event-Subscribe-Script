cc9f906500ea7da6abd92a77f24ba3f6f0ff20f2
Limm-jk <201602057@cs-cnu.org>
2021-02-09 23:57:40 +0900

    [feat] content list
    event 데이터를 추출, issue의 Body로 정리하는 함수. 과정에 필요한 find due day 함수도 정의

--- event_crawling.py
+++ event_crawling.py
@@ -25,3 +25,44 @@ def split_event_html(html, range = 20):
     br_split_HTML = list(html.split('<br>')[range:])
     soup = BeautifulSoup(br_split_HTML[0] + br_split_HTML[1], 'html.parser')
     return soup.findAll("li")
+
+def find_due_day(body):
+    """
+    이벤트에서 마감일자를 찾아줌
+    param body -> 이벤트 날짜 / soup Object
+    return -> arr
+    arr[0] = month
+    arr[1] = date
+    arr[2] = month&date
+    """
+    str_body = str(body)
+    dot_split_str = str_body.split('.')
+    """
+    expected
+    '<li>신청: 02', ' 04(목) 14:00 / 02', ' 05(금) 14:00</li>'
+    """
+    month = dot_split_str[-2][-2:]
+    day = dot_split_str[-1][1:3]
+    MnD = month + day
+    return [month,day,MnD]
+
+def content_list(events, day):
+    """
+    event 데이터를 추출, issue의 Body로 정리함.
+    param events -> 이벤트의 리스트, 쓰레기 데이터가 존재함. / soup Object List
+    param day -> 오늘 날짜. 마감된 날짜를 제거하는 기준이 됨. / int
+    
+    return str
+    """
+    current_content = '' # output
+    
+    for event in events:
+        event_body = event.findAll("li")
+        event_title = event.find("strong")
+        
+        if len(event_body) > 0: # 내용이 존재하는 Object만 연산
+            link = event_body[3].select("a")[0].attrs['href']
+            due_date = find_due_day(event_body[2])
+            if day <= int(due_date[2]):
+                content = f"[{event_title.text}]({link})" + "\n -" + event_body[2].text + "\n -"+ event_body[1].text + " <br/>\n "
+                current_content += content
