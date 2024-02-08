673048fe8b2a89ba625f897a363a60f0229946f2
고언약[KR Server Tech] <covenant.ko@kakao.com>
2024-02-08 16:54:47 +0900

    Update 행사 조회 방식 전환

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Dev-Event-Subscribe-Script
-Dev Event 메일링을 위한 파이썬 스크립트  
+> Dev Event 메일링을 위한 파이썬 스크립트  
+
+<br />
+
+limm-jk님의 후기: [Github Actions와 Issue로 간단하게 메일링 서비스 구현하기 - 'Dev-Event-Subscribe'](https://limm-jk.tistory.com/51)
+
--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import os
 from datetime import datetime
 from pytz import timezone
 from github import Github
+import requests
 
-from event_crawling import get_html, split_event_html, content_list
+def get_event():
+    API_PATH = os.environ['REAL_API']
+    response = requests.get(API_PATH)
+    if response.status_code != 200:
+        return ""
+    return response.text
 
 
 if __name__ == "__main__":
     access_token = os.environ['MY_GITHUB_TOKEN']
     repository_name = "Dev-Event-Subscribe"
-    event_url = 'https://github.com/brave-people/Dev-Event'
-    
-    # 스크립트 상단에 소개 및 공지문구
-    # markdown 형식으로 적어주세요.
-    script_title = '<div align=center> <img src="https://github.com/brave-people/Dev-Event/blob/master/static/title-v3-md.png?raw=true"> \n \n __더 많은 행사를 보고 싶다면? [Github Dev Event](https://github.com/brave-people/Dev-Event) 로 오세요!__ </div>'
 
     seoul_timezone = timezone('Asia/Seoul')
     today = datetime.now(seoul_timezone)
@@ -23,17 +24,15 @@ if __name__ == "__main__":
     
     today_int = int(today.strftime('%m%d'))
     
-    event_html = get_html(event_url)
-    event_object = split_event_html(event_html)
-    
     title = f"주간 Dev Event - {now_month}월 {week}째주 개발자 행사"
     
-    content = content_list(script_title, event_object, today_int)
-    
-    # repo에 접근
-    g = Github(access_token)
-    repo = g.get_organization("brave-people").get_repo(repository_name)
-    
-    repo.create_issue(title=title, body=content)
-    
-    print("Update 완료")
+    content = get_event()
+    if content != "":
+        # repo에 접근
+        g = Github(access_token)
+        repo = g.get_organization("brave-people").get_repo(repository_name)
+        
+        # 이슈 생성
+        repo.create_issue(title=title, body=content)
+        
+        print("issue 등록 완료")
